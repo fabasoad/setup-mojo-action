0f87a3af200fd594969e48bab5d2fe972f709cf3
Arun Sharma <arun@sharma-home.net>
2025-01-02 09:27:23 -0800

    mojo: backward compat script

--- src/install-magic.sh
+++ src/install-magic.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 
+# Installs magic and creates a mojo compatiblity script
+
 main() {
   curl -ssL https://magic.modular.com/8dc65f55-3ba2-410c-a46f-4f8f8102a7b7 | bash
   echo "$HOME/.modular/bin" >> "$GITHUB_PATH"
+  # so people can use mojo instead of magic run mojo
+  cat > "$HOME/.modular/bin/mojo" << EOF
+#!/bin/sh
+
+magic run mojo $*
+EOF
+  chmod a+rx "$HOME/.modular/bin/mojo"
 }
 
 main "$@"
