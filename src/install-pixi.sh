251e624d2aa686f80de22cdf450b5e64cf98286c
fabasoad <fabasoad@gmail.com>
2025-06-15 14:49:10 +0900

    Pixi script

--- src/install-pixi.sh
+++ src/install-pixi.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 main() {
-  curl -ssL https://magic.modular.com/8dc65f55-3ba2-410c-a46f-4f8f8102a7b7 | bash
-  echo "$HOME/.modular/bin" >> "$GITHUB_PATH"
+  curl -fsSL https://pixi.sh/install.sh | sh
 }
 
 main "$@"
